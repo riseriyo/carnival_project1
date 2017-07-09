a78d121bcd8ebc4c98f8ee41791a714f8b259243
riseriyo <riseriyo@gmail.com>
2017-07-09 13:46:32 -0700

    vrnd project 1: carnival

---
+++ Assets/UdacityVR/Scripts/CarnivalCoinToss.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CarnivalCoinToss : MonoBehaviour {
+
+    [Tooltip("Floating Score Prefab")]
+    public ScoreHighlight ScoreHighlighterPrefab;
+
+    [Tooltip("The Camera that the Player uses to see the world")]
+    public Camera VRView;
+
+    [Tooltip("Controls the scale of the power the coin is Tossed")]
+    public CarnivalPowerSlider PowerScalar;
+
+    [Tooltip("The prefab for the coin we are going to toss.")]
+    public GameObject CoinPrefab;
+
+    [Tooltip("The Center of the Coin Pile")]
+    public Transform CoinPile;
+
+    [Tooltip("The current coin we control")]
+    public float DistanceFromFace = .5f;
+
+    [Tooltip("The MINIMUM power toss the coin with. This is when the slider is at the bottom")]
+    public float MinTossPower = 4f;
+
+    [Tooltip("The MAXIMUM power toss the coin with. This is when the slider is at the top")]
+    public float MaxTossPower = 8f;
+
+    [Tooltip("The speed that the slider moves up and down with")]
+    public float SliderSpeed = 2f;
+
+    [Tooltip("The audio clip to play when coins are clicked")]
+    public GvrAudioSource clickNotification;
+
+    [Tooltip("The audio clip to play when the coin lands")]
+    public AudioSource yay;
+
+    [Tooltip("The audio clip to play when coin misses")]
+    public AudioSource fail;
+
+    private GameObject currCoin;
+
+    private bool coinPickedUp = false;
+    private float pickUpTime = 0f;
+
+    void Start() {
+        CarnivalCoinTossPlatform.OnCoinLanded += OnCoinLanded;
+        CoinTossCoin.OnCoinMissed += OnCoinMissed;
+    }
+
+    void Update() {
+        if (coinPickedUp) {
+            //HeadPose.position doesn't seem to work...
+            Vector3 target = VRView.transform.position + (VRView.transform.rotation * Vector3.forward) * DistanceFromFace;
+            Vector3 curr = currCoin.transform.position;
+            if ( (target - curr).magnitude > .01f ) {
+                currCoin.transform.position = Vector3.Lerp(curr, target, Time.deltaTime * 3f); //have the coin float to your head
+            }
+
+            PowerScalar.SetPowerScale( Mathf.Abs(Mathf.Sin( (Time.time - pickUpTime) * SliderSpeed )) );
+        }
+    }
+
+    public void PickUpCoin() {
+        if (!coinPickedUp) {
+            clickNotification.Play();
+            currCoin = Instantiate(CoinPrefab);
+            currCoin.GetComponent<Collider>().enabled = false;
+            currCoin.transform.position = CoinPile.position;
+            coinPickedUp = true;
+            pickUpTime = Time.time;
+        }
+    }
+
+	// Difficult task! But I was able to get scores with targetVel.normalized at (0.8, 0.6, 0.1) and power and r.velocity values at: 
+	// 6.72757, (5.2, 4.2, 1.0)
+	// 6.926415, (5.2, 4.2, 1.0)
+	// 7.350133, (5.6, 4.6, 1.1)
+	// to give you an idea that it is possible. Just make sure you set the GVRViewCamera closer to the coin toss booth. It seems to help.
+    public void TossCoin() {
+        if (coinPickedUp) { 
+            coinPickedUp = false;
+            currCoin.GetComponent<Collider>().enabled = true;
+            currCoin.GetComponent<GvrAudioSource>().Play();
+            Rigidbody r = currCoin.GetComponent<Rigidbody>();
+            r.isKinematic = false;
+			//Debug.Log("VRView.transform.position: " + VRView.transform.position + "; VRView.transform.forward: " + VRView.transform.forward); // (0.1, 1.8, -1.1)
+			//VRView.transform.forward = new Vector3(-1f, 1f, 0.8f);
+			//Debug.Log("VRView.transform.forward: " + VRView.transform.forward);
+			Vector3 targetVel = VRView.transform.position + VRView.transform.forward; //the direction of the toss
+			//Debug.Log("targetVel: " + targetVel);
+            targetVel.y = 0f;
+			//Debug.Log ("targetVel before normalized: " + targetVel);
+            targetVel.Normalize();
+			//Debug.Log ("targetVel after normalized " + targetVel);
+            targetVel.y = 0.8f; //0.8f have a consistant y velocity
+			//Debug.Log ("targetVel after y = 0.8f:  " + targetVel);
+            float power = (MaxTossPower - MinTossPower) * PowerScalar.value + MinTossPower;
+			r.velocity = targetVel.normalized * power;
+			Debug.Log("Points scored w/values: (targetVel.normalized, power, r.velocity): (" + targetVel.normalized + ", " + power + ", " + r.velocity + ")");
+            currCoin = null;
+        }
+    }
+
+    private void OnCoinLanded() {
+        CarnivalScores.Instance.IncrementCoinScore();
+        ScoreHighlight sh = Instantiate(ScoreHighlighterPrefab, transform.position,
+    Quaternion.LookRotation(-transform.right));
+        sh.SetPoints(1000);
+
+        yay.Play();
+
+#if UNITY_EDITOR
+        TMPro.TextMeshPro text = new GameObject().AddComponent<TMPro.TextMeshPro>();
+        text.transform.rotation = transform.rotation * Quaternion.Euler(0f, 270f, 0f);
+        text.transform.position = new Vector3(7.5f, 6f, -2f);
+        text.text = "OssDist0.7";
+#endif
+    }
+
+    private void OnCoinMissed() {
+        ScoreHighlight sh = Instantiate(ScoreHighlighterPrefab, transform.position,
+    Quaternion.LookRotation(-transform.right));
+        sh.SetPoints(0);
+
+        fail.Play();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,55 @@
+# Student Name: Rise Riyo
+# Project 1: Carnival+ 
+# Date: July 9, 2017
+
+* Included files/folders:
+ * - README.md
+ * - Unity Assets folder
+ * - Project Settings folder
+
+* Versions:
+ * GVR: 1.5
+ * Unity: 5.6.1p2
+
+* +Note: I am a self-paced student resuming the VRND Program. I had to stop the program when I was on VR Design so I had already
+completed Project 1: First VR App and had turned it in, but it was not being displayed that I had completed Project 1 in my account. So
+I did the Carnival Project thinking that I was responsible for doing it; although Martin Witter and Karen Dragos said I didn't need to after
+I spoken to them. But I'll submit the Carnival Project and use it as a refresher for me. Thanks. 
+
+# Work Duration: 
+* This project took me approximately 15 hours to complete.
+
+# Challenges
+* Coin Toss: I understand the intent of this lesson was to get us to explore/test out various values for both Min and Max Toss Power. I tried a gazillion combinations of values. When I found the secret code for the Coin Toss to be "CTMin10Max13", I started to play with those values. But I found
+that the direction of the coins was to the right of the Coin Booth. I started to tweak the CarnivalCoinToss.cs file, particularly TossCoin(), thinking
+that I could plug in some numbers for the direction of the coin, but that only made my coin either go directly behind the Player or again to the
+right of the booth. So after reading the forum, I made my min single digits and kept my max at 13, moved my GRViewer closer to the booth, moved the
+Power Slider to the left of the booth, and was able to score pretty consistently with the given variable values:
+ * targetVel.normalized: (0.8, 0.6, 0.1)
+ * power: between 6.72751 - 7.350133
+ * r.velocity: between (5.2, 4.2, 1.0) to (5.6, 4.6, 1.1)
+* Sorry, but I was very confused on the term, "Wiring up". I didn't know if the lesson's author meant by working with the script or something completely different. And when looking at the "directions" in the lesson description, I didn't notice what perhaps 'wiring' was referring to other than the Plinko, but again it was vague to me. BTW, was there a way to tweak the scripts so that the coin landed -- for the most part -- on the Coin booth counter? Just curious. 
+* The remaining tasks were straightforward.  
+
+# Extra Features
+- Added fireworks based on the video, "How To: Fireworks in Unity". Credit: Tyler Wissler at https://youtu.be/fySBsYh5GsM as of July 9, 2017.
+- Attempted to add in a very simple, and not very pretty, StarterScene. It seems to work in Unity, but it's not deployed on my Android. I was trying to mimic what Project 3: Puzzler did with the canvas as a starter scene, but I didn't have time to figure that out. I'll look into it later, but any tips on how to make my canvas work, please let me know.
+- I would like to add in a Timer, but I'm not sure what you guys mean by that. Do you mean, Player has so many seconds to play all games and then it
+resets? Or something like that? 
+
+# Rating of Project 1: Carnival
+* Good for me to do because it became a refresher course. I listened to all of the vids as well, and most likely will continue doing reviewing 
+things in the new platform since my old completed projects (1 through 3) are built for Unity 5.5.0f3 with GRViewer v1.0. 
+
+# References
+* Albreda, Zak. "Change Scenes in Unity 5 C#". https://zakalberda.com/2017/03/13/change-scenes-in-unity-5-c/ as of July 9, 2017.
+* Albreda, Zak. "Create at start menu/Change scenes in unity 5!". https://www.youtube.com/watch?v=TEq4P0kDAtE as of July 9, 2017.
+* Wissler, Tyler. "How To: Fireworks in Unity". https://youtu.be/fySBsYh5GsM as of July 9, 2017.
+
+
+
+
+
+
+
+
